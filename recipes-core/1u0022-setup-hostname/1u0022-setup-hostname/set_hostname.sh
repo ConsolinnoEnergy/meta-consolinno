6b8f5faf41363c2809965b6fa7aacadcc4d9a4fe
l.heizinger <l.heizinger@consolinno.de>
2024-03-22 08:41:24 +0100

    Directly mount bind /etc/hostname to /srv/hostame

--- recipes-core/1u0022-setup-hostname/1u0022-setup-hostname/set_hostname.sh
+++ recipes-core/1u0022-setup-hostname/1u0022-setup-hostname/set_hostname.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
 
 if [ -f /srv/hostname ]; then
-    NEW_HOSTNAME=$(cat /srv/hostname)
+    mount --bind /srv/hostname /etc/hostname
 else
     echo "No hostname file found in /srv/hostname"
     exit 1
 fi
-
-echo $NEW_HOSTNAME > /tmp/hostname
-mount --bind /tmp/hostname /etc/hostname
